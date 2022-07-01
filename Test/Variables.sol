7696be19f52c8d6033a07c7a30bae8843a7c57b9
Emperor Toni <75282278+EmperorToni@users.noreply.github.com>
2022-07-01 14:54:41 +0200

    Add files via upload

---
+++ Test/Variables.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0; 
+
+contract Variables { 
+
+    uint256 FavoriteNumber = 10; 
+
+    bool FavoriteBool = true; 
+
+    string FavoriteString = "String"; 
+
+    int256 FavoriteInt = -10; 
+
+    address FavoriteAddress = 0xDB21df4055b27E14A82E320fa6ba2Cc4dD227C94; 
+
+    bytes32 FavoriteBytes = "bytes";
+}
